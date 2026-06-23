b7513420c0244b4047f1e54281e00e5b0091012a
Jessica <jessicanoleen@gmail.com>
2026-06-23 10:11:14 +0700

    sucks

--- articles/blob/fetch.js
+++ articles/blob/fetch.js
@@ -5,7 +5,7 @@ document.addEventListener('click', (e) => {
 
     const slug = link.getAttribute('href').split('/').pop().replace(/\.md$/, '');
     
-    fetch(`/public/articles-manifest.json`)
+    fetch(`/json/rticles-manifest.json`)
         .then(res => res.json())
         .then(manifest => {
             const path = manifest[slug];
